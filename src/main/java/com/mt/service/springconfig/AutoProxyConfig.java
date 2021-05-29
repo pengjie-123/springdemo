61334d785ee41a4c92f627cb209cfa396b450042
Jie Peng <czxiaoshu@163.com>
2021-05-29 21:16:48 +0800

    add Dockerfile compose

---
+++ src/main/java/com/mt/service/springconfig/AutoProxyConfig.java
@@ -0,0 +1,9 @@
+package com.mt.service.springconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AutoProxyConfig {
+}
